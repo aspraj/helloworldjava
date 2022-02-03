ce932c7aa82a19e81aeb49594c811cad9ac12ade
Aman singh <amansinghpatel429@gmail.com>
2022-02-03 20:07:28 +0530

    Hello sir i am sharing my data with you

---
+++ src/helloworldecllipse/HelloWorld.java
@@ -0,0 +1,17 @@
+package helloworldecllipse;
+
+/**
+ * @author RAJPUT JI
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Aman Singh Patel");
+	}
+
+}
